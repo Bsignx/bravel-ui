56fdf8357e05982640227380d2ae613547990347
Bruno Mariano <bsignx@gmail.com>
2022-01-25 20:27:59 -0300

    chore: add prettier-plugin-tailwindcss

--- src/toggle-switch.tsx
+++ src/toggle-switch.tsx
@@ -15,13 +15,13 @@ export const ToggleSwitch = ({
     checked={enabled}
     onChange={onChange}
     className={`${enabled ? 'bg-green-500' : 'bg-gray-400'}
-          relative inline-flex flex-shrink-0 h-7 w-14 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+          relative inline-flex h-7 w-14 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
   >
     <span className="sr-only">Use setting</span>
     <span
       aria-hidden="true"
       className={`${enabled ? 'translate-x-7' : 'translate-x-0'}
-            pointer-events-none inline-block h-6 w-6 rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200`}
+            pointer-events-none inline-block h-6 w-6 transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
     />
   </Switch>
 )
