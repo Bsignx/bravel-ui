1617728afa7da93d3b663cef04f4805109008bc6
Bruno Mariano <bsignx@gmail.com>
2022-01-25 21:03:45 -0300

    chore: add custom tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,26 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './stories/*'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      gray900: '#111827',
+      gray50: '#F9FAFB',
+      fuchsia500: '#D946EF',
+      orange500: '#F97316',
+      green500: '#22C55E',
+      rose500: '#F43F5E',
+      gray700: '#374151',
+      gray500: '#6B7280',
+    },
+    fontFamily: {
+      sans: ['Rubik', 'sans-serif'],
+    },
   },
   variants: {},
   plugins: [],
-};
+}
