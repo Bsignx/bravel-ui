5622aa8887b73b2cca6404a657ad4c8db0c810d3
Bruno Mariano <bsignx@gmail.com>
2022-02-09 06:45:25 -0300

    fix: update example

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,15 @@
-import 'react-app-polyfill/ie11';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import 'react-app-polyfill/ie11'
+
+import { Button } from '../src/button'
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <Button>Example</Button>
     </div>
-  );
-};
+  )
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
